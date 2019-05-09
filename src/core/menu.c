017b6eb3900464c9962ce5a2a3f3dd4a65b59fb7
MrCesar107 <cesar.rodriguez.lara54@gmail.com>
2019-05-09 00:11:03 -0500

    First part of menu interface

---
+++ src/core/menu.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Custom headers
+#include "menu.h"
+
+// Funcion que pinta la interfaz de los menus
+void menu (char titulo[100]) {
+  int x = 0;
+  int y = 0;
+
+  for (y = 0; y < HEIGHT; y++) {
+    for (x = 0; x < WIDTH; x++) {
+      if (y == 0 || y == HEIGHT - 1 || x == 0 || x == WIDTH - 1) {
+        printf("*");
+      } else {
+        printf(" ");
+      }
+    }
+
+    printf("\n");
+  }
+}
---
+++ src/core/menu.h
@@ -0,0 +1,11 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+// Custom constants
+#define WIDTH       80
+#define HEIGHT      20
+
+// Header methods
+void menu(char titulo[100]);
+
+#endif // MENU_H_
